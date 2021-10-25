5559c78626f1bfda72e754ea64842da011ba77fd
Caio <caiovictorsalesdefreitas@gmail.com>
2021-10-25 17:06:57 -0300

    Inicio de QtDesign

---
+++ ui/controle.py
@@ -0,0 +1,22 @@
+from PyQt5 import uic, QtWidgets
+
+def funcao_principal():
+    if formulario.radioButton.isChecked():
+        opcao = "Opção 1 selecionada"
+    elif formulario.radioButton_2.isChecked():
+        opcao = "Opção 2 selecionada"
+    elif formulario.radioButton_3.isChecked():
+        opcao = "Opção 3 selecionada"
+    elif formulario.radioButton_4.isChecked():
+        opcao = "Opção 4 selecionada"
+    else:
+        print("")
+
+    formulario.label_2.setText(opcao)
+
+app = QtWidgets.QApplication([])
+formulario = uic.loadUi("janela.ui")
+formulario.pushButton.clicked.connect(funcao_principal)
+
+formulario.show()
+app.exec()
