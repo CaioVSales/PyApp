dcf9acebd958436a268dada9cad2ac788983fef6
Caio <caiovictorsalesdefreitas@gmail.com>
2021-10-25 18:28:03 -0300

    Novas Alterações

---
+++ ui/verificar/controle.py
@@ -0,0 +1,21 @@
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+
+def exibir_mensagem():
+    dado_lido = janela.lineEdit.text()
+    print(dado_lido)
+    janela.lineEdit.setText('')
+    if dado_lido == "":
+        QMessageBox.about(janela,"alerta","Nenhum nome inserido.")
+    else:
+        QMessageBox.about(janela,"alerta","Olá, " + dado_lido)
+
+
+
+app = QtWidgets.QApplication([])
+janela = uic.loadUi("verificar.ui")
+janela.pushButton.clicked.connect(exibir_mensagem)
+
+janela.show()
+app.exec()
